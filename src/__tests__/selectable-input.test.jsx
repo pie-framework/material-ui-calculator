9f5ff8edf1e482c358b6a67bf9c09f9a8ed469f0
edeustace <ed.eustace@gmail.com>
2018-03-09 13:31:50 -0500

    chore(selectable-input): tests

--- src/__tests__/selectable-input.test.jsx
+++ src/__tests__/selectable-input.test.jsx
@@ -3,24 +3,109 @@ import React from 'react';
 import SelectableInput from '../selectable-input';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
+import TextField from 'material-ui/TextField';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('selectable-input', () => {
-  it('focus', () => {
+let mockInput, props;
 
-    const props = {
+describe('selectable-input', () => {
+
+  const mkWrapper = () => {
+    props = {
       inputRef: jest.fn(),
       onSelectionChange: jest.fn(),
       onChange: jest.fn(),
-      value: 'test'
+      value: 'test',
+      inputRef: jest.fn(),
+      onKeyDown: jest.fn()
     }
 
-    const wrapper = shallow(<SelectableInput {...props} />);
-    const mockInput = { focus: jest.fn() }
-    wrapper.instance().input = mockInput;
-    wrapper.instance().focus();
-    expect(mockInput.focus.mock.calls.length).toEqual(1);
+    const out = shallow(<SelectableInput {...props} />);
 
-  })
+    mockInput = {
+      focus: jest.fn(),
+      selectionStart: 1,
+      selectionEnd: 2
+    }
+
+    out.instance().inputRef(mockInput);
+    return out;
+  }
+
+  describe('TextField', () => {
+
+    it('disabledUnderline: true', () => {
+      const wrapper = mkWrapper();
+      expect(wrapper.find(TextField).prop('InputProps')).toMatchObject({
+        disableUnderline: true
+      });
+    });
+  });
+
+  describe('focus', () => {
+    it('calls input.focus', () => {
+      const wrapper = mkWrapper();
+      wrapper.instance().focus();
+      expect(mockInput.focus.mock.calls.length).toEqual(1);
+    });
+  });
+
+  describe('onKeyUp', () => {
+
+    it('calls onSelectionChange', () => {
+      const wrapper = mkWrapper();
+      wrapper.find(TextField).prop('onKeyUp')();
+      expect(props.onSelectionChange.mock.calls[0][0]).toEqual({
+        selectionStart: 1,
+        selectionEnd: 2
+      });
+    });
+  });
+
+  describe('onChange', () => {
+    it('calls onChange', () => {
+
+      const wrapper = mkWrapper();
+      wrapper.find(TextField).prop('onChange')({ target: { value: 'v', selectionStart: 0, selectionEnd: 1 } });
+
+      expect(props.onChange.mock.calls[0][0]).toEqual({
+        target: {
+          value: 'v',
+          selectionStart: 0,
+          selectionEnd: 1
+        }
+      });
+    });
+  });
+
+  describe('onClick', () => {
+
+    it('calls selectionChange', () => {
+      const wrapper = mkWrapper();
+      wrapper.find(TextField).prop('onClick')();
+      expect(props.onSelectionChange.mock.calls[0][0]).toEqual({
+        selectionStart: 1,
+        selectionEnd: 2
+      });
+    });
+  });
+
+  describe('inputRef', () => {
+    it('calls props.inputRef', () => {
+      expect(props.inputRef.mock.calls.length).toEqual(1);
+    });
+  });
+
+  describe('onKeyDown', () => {
+    it('calls props.onKeyDown', () => {
+      const wrapper = mkWrapper();
+      const e = {
+        target: { value: 'target' }
+      }
+
+      wrapper.find(TextField).prop('onKeyDown')(e);
+      expect(props.onKeyDown.mock.calls[0][0]).toEqual(e);
+    });
+  });
 });
